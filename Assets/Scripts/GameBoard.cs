8466eedc6eabd63383c5c9c17f26897202ad07fd
Johan <56724011+risujas@users.noreply.github.com>
2024-06-22 13:57:53 +0300

    Add GameBoard.cs

---
+++ Assets/Scripts/GameBoard.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class GameBoard : MonoBehaviour
+{
+	[SerializeField] private int boardSizeX = 8;
+	[SerializeField] private int boardSizeY = 8;
+
+	private void Start()
+	{
+
+	}
+
+	private void Update()
+	{
+
+	}
+}
