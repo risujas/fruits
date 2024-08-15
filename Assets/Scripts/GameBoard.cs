51861e614f73523087ebc60c16506971a3f47007
Johan <56724011+risujas@users.noreply.github.com>
2024-08-15 20:49:35 +0300

    Create board slots

--- Assets/Scripts/GameBoard.cs
+++ Assets/Scripts/GameBoard.cs
@@ -1,17 +1,40 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class GameBoard : MonoBehaviour
 {
 	[SerializeField] private int boardSizeX = 8;
 	[SerializeField] private int boardSizeY = 8;
+	[SerializeField] private GameObject slotPrefab;
+
+	private List<GameObject> slots;
+
+	private void Create()
+	{
+		slots = new List<GameObject>();
+		for (int x = 0; x < boardSizeX; x++)
+		{
+			for (int y = 0; y < boardSizeY; y++)
+			{
+				var newSlot = Instantiate(slotPrefab, new Vector3(x, y), Quaternion.identity, transform);
+				slots.Add(newSlot);
+			}
+		}
+	}
+
+	private void SetOffset()
+	{
+		transform.position -= transform.right * (boardSizeX / 2.0f);
+		transform.position -= transform.up * (boardSizeY / 2.0f);
+	}
 
 	private void Start()
 	{
-
+		Create();
+		SetOffset();
 	}
 
 	private void Update()
 	{
-
 	}
 }
