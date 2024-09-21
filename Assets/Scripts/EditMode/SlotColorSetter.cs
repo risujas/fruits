9a62b1dde56aad28644e62f4c347e6aea79f5638
Johan <56724011+risujas@users.noreply.github.com>
2024-09-21 18:03:46 +0300

    Add SlotColorSetter

---
+++ Assets/Scripts/EditMode/SlotColorSetter.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class SlotColorSetter : MonoBehaviour
+{
+	[SerializeField] private SpriteRenderer background;
+
+	public void SetBackgroundColor()
+	{
+		SlotItem item = GetComponentInChildren<SlotItem>();
+		if (item)
+		{
+			background.color = item.ItemColor;
+		}
+		else
+		{
+			background.color = Color.white;
+		}
+	}
+
+	private void Update()
+	{
+		SetBackgroundColor();
+	}
+}
--- Assets/Scripts/Slot.cs
+++ Assets/Scripts/Slot.cs
@@ -1,25 +1,5 @@
 using UnityEngine;
 
-[ExecuteInEditMode]
 public class Slot : MonoBehaviour
 {
-	[SerializeField] private SpriteRenderer background;
-
-	public void SetBackgroundColor()
-	{
-		SlotItem item = GetComponentInChildren<SlotItem>();
-		if (item)
-		{
-			background.color = item.ItemColor;
-		}
-		else
-		{
-			background.color = Color.white;
-		}
-	}
-
-	private void Update()
-	{
-		SetBackgroundColor();
-	}
 }
