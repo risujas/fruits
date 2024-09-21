32b197f579a94c0ee1c999907e85a0842a4a1c5b
Johan <56724011+risujas@users.noreply.github.com>
2024-09-21 18:20:22 +0300

    Add InputHandler

---
+++ Assets/Scripts/InputHandler.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class InputHandler : MonoBehaviour
+{
+	private Slot selectedSlot;
+
+	private void HandleInput()
+	{
+		if (Input.GetMouseButtonUp(0))
+		{
+
+		}
+	}
+
+	private void Update()
+	{
+		HandleInput();
+	}
+}
