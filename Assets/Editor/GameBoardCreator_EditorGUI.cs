8d84ce80b4485b0da38cfe5f046e1f9b388cdd25
Johan <56724011+risujas@users.noreply.github.com>
2024-10-03 16:41:24 +0300

    Update GameBoardCreator_EditorGUI.cs

--- Assets/Editor/GameBoardCreator_EditorGUI.cs
+++ Assets/Editor/GameBoardCreator_EditorGUI.cs
@@ -13,26 +13,11 @@ public class GameBoardCreator_EditorGUI : Editor
 
 		GameBoardCreator gameBoardCreator = (GameBoardCreator)target;
 
-		GUILayout.Label("\nInitialization");
-
 		if (GUILayout.Button("Create Board Instance"))
 		{
 			gameBoardCreator.CreateBoard();
-		}
-
-		if (GUILayout.Button("Create Slots"))
-		{
 			gameBoardCreator.CreateSlots();
 			gameBoardCreator.SetOffset();
-		}
-
-		if (GUILayout.Button("Destroy Slots"))
-		{
-			gameBoardCreator.DestroySlots();
-		}
-
-		if (GUILayout.Button("Set Camera Size"))
-		{
 			gameBoardCreator.SetCameraSize();
 		}
 
